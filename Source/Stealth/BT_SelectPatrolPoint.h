75d588a39be84f305c6d7e8a175682e6ed27b549
carlso70 <carlso70@purdue.edu>
2016-06-01 18:06:38 -0500

    first commit

---
+++ Source/Stealth/BT_SelectPatrolPoint.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "BehaviorTree/BTTaskNode.h"
+#include "BT_SelectPatrolPoint.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STEALTH_API UBT_SelectPatrolPoint : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
